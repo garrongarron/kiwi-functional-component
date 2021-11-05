6a20429c1f3563cc71b6206e04f4e9a8c8fc8873
Samu Garron <garron.garron@gmail.com>
2021-11-05 01:09:56 -0300

    Swaping React by Kiwi JS

--- react/threeCol/LearnOnce.js
+++ react/threeCol/LearnOnce.js
@@ -3,7 +3,7 @@ export default function LearnOnce(){
     <div class="card shadow-sm border-dark">
         <div class="card-body k-bg-4">
             <h3 class="k-color-2 fw-light">Learn Once, Write Anywhere</h3>
-            <p class="card-text text-light">We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.</p>
+            <p class="card-text text-light">We don’t make assumptions about the rest of your technology stack, so you can develop new features in Kiwi JS without rewriting existing code.</p>
             <p class="card-text text-light">It would be easy render on the server using Node and power mobile apps using this library.</p>
         </div>
     </div>
